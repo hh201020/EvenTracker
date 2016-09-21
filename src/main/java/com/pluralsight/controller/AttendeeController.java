81941dad359dd2b7d15510c94911e1c085e24dff
Jerry Hu <hh201020@gmail.com>
2016-09-21 14:49:16 -0400

    Adding validation

--- src/main/java/com/pluralsight/controller/AttendeeController.java
+++ src/main/java/com/pluralsight/controller/AttendeeController.java
@@ -1,10 +1,12 @@
 package com.pluralsight.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.pluralsight.model.Attendee;
 
@@ -19,8 +21,11 @@ public class AttendeeController {
 	}
 	
 	@RequestMapping(value="/attendee", method=RequestMethod.POST)
-	public String processAttendee(@ModelAttribute("attendee") Attendee attendee) {
+	public String processAttendee(@Valid Attendee attendee, BindingResult result, Model model) {
 		System.out.println(attendee);
+		if(result.hasErrors()){
+			return "attendee";
+		}
 		return "redirect:index.html";
 	}
 }
